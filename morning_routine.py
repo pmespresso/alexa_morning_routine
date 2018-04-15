44efcc0a4d08fa3a9e0a40e51932b668857a206d
YJ <yjkimjunior@gmail.com>
2018-04-14 19:43:20 -0700

    intents

--- morning_routine.py
+++ morning_routine.py
@@ -14,12 +14,21 @@ logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 
 @ask.launch
-
 def new_game():
 
     welcome_msg = render_template('welcome')
 
     return question(welcome_msg)
+
+@ask.intent("MissionAccomplishedIntent")
+def next_routine():
+
+    are_you_done = render_template('are_you_done')
+
+    return question(are_you_done)
+
+@ask.intent("")
+
 
 
 if __name__ == '__main__':
